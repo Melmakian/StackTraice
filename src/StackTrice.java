c1035bc2ad4ea4351417fe9fb032f00a6feb1c75
Alf <siidousdart@gmail.com>
2022-11-28 01:56:10 +0500

    add ALL

---
+++ src/StackTrice.java
@@ -0,0 +1,19 @@
+public class StackTrice {
+    public static void main(String[] args) {
+        Thread thread = new Thread(new SpecialThread() );
+        thread.start();
+        System.out.println("*******************");
+        for (StackTraceElement element : Thread.currentThread().getStackTrace())
+            System.out.println(element);
+    }
+    public static class SpecialThread implements Runnable{
+        @Override
+        public void run(){
+            for (StackTraceElement element : Thread.currentThread().getStackTrace())
+                System.out.println(element);
+
+            }
+        }
+    }
+
+
